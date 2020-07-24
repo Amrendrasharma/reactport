c9372f78c4033c09b47831ace50d2891a790c7bd
amrendra pratap sharma <amrendrasharmasrh@gmail.com>
2020-07-24 19:44:17 +0530

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ class Content extends Component {
   render() {
     return (
       <div className="container">
-        <h1 style={{ fontSize: "40px" }}>Bharat Singh</h1>
+        <h1 style={{ fontSize: "40px" }}>Amrendra pratap sharma</h1>
         <h1>
           <ReactTypingEffect
             className="typingeffect"
